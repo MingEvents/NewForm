c1eec0fc72ad3eae5df20b9e4e4d2645c4620414
Aimadpro <aaisad2324@politecnics.barcelona>
2025-05-09 19:36:50 +0200

    EWFGRF

---
+++ WpfApp1/Model/Managment/Orm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+using WpfApp1.Model;
+
+namespace TogetherNotes.Models
+{
+    class Orm
+    {
+        public static MingsEventsEntities db = new MingsEventsEntities();
+
+        public static string ErrorMessage(SqlException sqlException)
+        {
+            string message = "";
+
+            switch (sqlException.Number)
+            {
+                case 2:
+                    message = "The server is not operational.";
+                    break;
+                case 547:
+                    message = "The record cannot be deleted because it has related records.";
+                    break;
+                case 4060:
+                    message = "Could not connect to the database.";
+                    break;
+                case 18456:
+                    message = "Login failed.";
+                    break;
+                case 2601:
+                    message = "A record with the same value already exists.";
+                    break;
+                default:
+                    message = sqlException.Number + " - " + sqlException.Message;
+                    break;
+            }
+
+            return message;
+        }
+    }
+}
